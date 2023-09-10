f436fcf30a1c07a8a6f840ed4d3e93698fbc1b47
Johnremy Agbor <125705923+remy01gh@users.noreply.github.com>
2023-09-10 18:29:53 +0100

    Changed format of utcTime

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/api', (req, res) => {
 
   // Get the current UTC time (accurate within +/-2 minutes)
   const now = new Date();
-  const utcTime = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString();
+  const utcTime = now.toISOString();
 
   // Hardcoded GitHub URLs
   const githubFileUrl = 'https://github.com/remy01gh/endpoint-HNG/blob/main/app.js';
